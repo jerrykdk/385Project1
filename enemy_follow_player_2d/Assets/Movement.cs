7fde14389f7880ed769e3cf780206f1e754a682a
jerrykdk <59629229+jerrykdk@users.noreply.github.com>
2021-04-09 19:56:22 -0700

    newup

---
+++ enemy_follow_player_2d/Assets/Movement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float speed = 10f;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("q"))
+        {
+            pos.x -= speed * Time.deltaTime;
+            pos.y += speed * Time.deltaTime;
+        }
+          if (Input.GetKey("e"))
+        {
+            pos.x += speed * Time.deltaTime;
+            pos.y += speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("w"))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("s"))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+           if (Input.GetKey("z"))
+        {
+            pos.x -= speed * Time.deltaTime;
+            pos.y -= speed * Time.deltaTime;
+        }
+
+           if (Input.GetKey("x"))
+        {
+            pos.x += speed * Time.deltaTime;
+            pos.y -= speed * Time.deltaTime;
+        }
+        transform.position = pos;
+    }
+}
